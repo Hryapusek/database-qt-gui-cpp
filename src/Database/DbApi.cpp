c78b4dda77f42be7d11c1ee7fc1b82a36abd533e
Abraamyan Alexander <hryapusek@gmail.com>
2023-10-08 01:19:58 +0300

    new: remove update database api

--- src/Database/DbApi.cpp
+++ src/Database/DbApi.cpp
@@ -10,50 +10,109 @@
 #include <odb/database.hxx>
 #include <odb/core.hxx>
 
+namespace
+{
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::exception
+  template < class T >
+  void removeObject(typename T::Id_t id)
+  {
+    odb::transaction tr(DatabaseConnection::connection()->begin());
+    DatabaseConnection::connection()->template erase< T >(id);
+    tr.commit();
+    return;
+  }
+
+  /// @throw odb::exception
+  template < class T >
+  std::vector< T > getObjects()
+  {
+    odb::transaction tr(DatabaseConnection::connection()->begin());
+    auto result = DatabaseConnection::connection()->template query< T >();
+    typename std::vector< T > objects;
+    std::move(result.begin(), result.end(), std::back_inserter(objects));
+    tr.commit();
+    return objects;
+  }
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::database_exception if data is not valid
+  /// @throw odb::exception
+  template < class T >
+  void updateObject(T &&obj)
+  {
+    odb::transaction tr(DatabaseConnection::connection()->begin());
+    auto objToUpdate = DatabaseConnection::connection()->load< T >(obj.id());
+    *objToUpdate = std::forward< std::decay_t< T > >(obj);
+    DatabaseConnection::connection()->update(objToUpdate);
+    tr.commit();
+  }
+}
+
 std::vector< Person > DbApi::getPersons()
 {
-  odb::transaction tr(DatabaseConnection::connection()->begin());
-  auto result = DatabaseConnection::connection()->query< Person >();
-  std::vector< Person > persons;
-  std::move(result.begin(), result.end(), std::back_inserter(persons));
-  tr.commit();
-  return persons;
+  return getObjects< Person >();
 }
 
 std::vector< Auto > DbApi::getAutos()
 {
-  odb::transaction tr(DatabaseConnection::connection()->begin());
-  auto result = DatabaseConnection::connection()->query< Auto >();
-  std::vector< Auto > autos;
-  std::move(result.begin(), result.end(), std::back_inserter(autos));
-  tr.commit();
-  return autos;
+  return getObjects< Auto >();
 }
 
 std::vector< Route > DbApi::getRoutes()
 {
-  odb::transaction tr(DatabaseConnection::connection()->begin());
-  auto result = DatabaseConnection::connection()->query< Route >();
-  std::vector< Route > routes;
-  std::move(result.begin(), result.end(), std::back_inserter(routes));
-  tr.commit();
-  return routes;
+  return getObjects< Route >();
 }
 
 std::vector< JournalRow > DbApi::getJournalRows()
 {
-  odb::transaction tr(DatabaseConnection::connection()->begin());
-  auto result = DatabaseConnection::connection()->query< JournalRow >();
-  std::vector< JournalRow > journalRows;
-  std::move(result.begin(), result.end(), std::back_inserter(journalRows));
-  tr.commit();
-  return journalRows;
+  return getObjects< JournalRow >();
 }
 
 void DbApi::removePerson(Person::Id_t id)
 {
-  odb::transaction tr(DatabaseConnection::connection()->begin());
-  DatabaseConnection::connection()->erase<Person>(id);
-  tr.commit();
+  removeObject< Person >(id);
+  return;
+}
+
+void DbApi::removeAuto(Auto::Id_t id)
+{
+  removeObject< Auto >(id);
+  return;
+}
+
+void DbApi::removeRoute(Route::Id_t id)
+{
+  removeObject< Route >(id);
+  return;
+}
+
+void DbApi::removeJournalRow(JournalRow::Id_t id)
+{
+  removeObject< JournalRow >(id);
+  return;
+}
+
+void DbApi::updatePerson(Person person)
+{
+  updateObject< Person >(std::move(person));
+  return;
+}
+
+void DbApi::updateAuto(Auto autoObj)
+{
+  updateObject< Auto >(std::move(autoObj));
+  return;
+}
+
+void DbApi::updateRoute(Route route)
+{
+  updateObject< Route >(std::move(route));
+  return;
+}
+
+void DbApi::updateJournalRow(JournalRow journalRow)
+{
+  updateObject< JournalRow >(std::move(journalRow));
   return;
 }
--- src/Database/DbApi.hpp
+++ src/Database/DbApi.hpp
@@ -2,6 +2,7 @@
 #define DB_API_HPP
 
 #include <vector>
+
 #include "Person.hpp"
 #include "Auto.hpp"
 #include "Route.hpp"
@@ -10,21 +11,53 @@
 class DbApi
 {
 public:
-  /// @throw odb::exception if something goes wrong
+  /// @throw odb::exception
   static std::vector< Person > getPersons();
 
-  /// @throw odb::exception if something goes wrong
+  /// @throw odb::exception
   static std::vector< Auto > getAutos();
 
-  /// @throw odb::exception if something goes wrong
+  /// @throw odb::exception
   static std::vector< Route > getRoutes();
 
-  /// @throw odb::exception if something goes wrong
+  /// @throw odb::exception
   static std::vector< JournalRow > getJournalRows();
 
   /// @throw odb::object_not_persistent if person with given id not found
-  /// @throw odb::exception if something goes wrong
+  /// @throw odb::exception
   static void removePerson(Person::Id_t id);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::exception
+  static void removeAuto(Auto::Id_t id);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::exception
+  static void removeRoute(Route::Id_t id);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::exception
+  static void removeJournalRow(JournalRow::Id_t id);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::database_exception if data is not valid
+  /// @throw odb::exception
+  static void updatePerson(Person person);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::database_exception if data is not valid
+  /// @throw odb::exception
+  static void updateAuto(Auto autoObj);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::database_exception if data is not valid
+  /// @throw odb::exception
+  static void updateRoute(Route route);
+
+  /// @throw odb::object_not_persistent if person with given id not found
+  /// @throw odb::database_exception if data is not valid
+  /// @throw odb::exception
+  static void updateJournalRow(JournalRow journalRow);
 };
 
 #endif
