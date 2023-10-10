de0ddec7a3f7a534104d3b7e835d14d498373e88
Abraamyan Alexander <hryapusek@gmail.com>
2023-10-10 23:03:42 +0300

    new: personTab context menu realization

--- src/MainWindow/Person/PersonTab.cpp
+++ src/MainWindow/Person/PersonTab.cpp
@@ -5,10 +5,19 @@
 #include "AddPersonDialog.hpp"
 #include "RemovePersonDialog.hpp"
 
+#ifndef _NDEBUG
+  #include <iostream>
+#endif
+
 #include <optional>
 #include <odb/exceptions.hxx>
 #include <QMessageBox>
-
+#include <QMenu>
+#include <QAction>
+#include <QGuiApplication>
+#include <QClipboard>
+#include <algorithm>
+#include <ranges>
 
 namespace
 {
@@ -33,6 +42,109 @@ PersonTab::PersonTab(QTableWidget *table, QPushButton *addBtn, QPushButton *remo
   QObject::connect(addBtn_, &QPushButton::clicked, this, &PersonTab::addBtnClicked);
   QObject::connect(removeBtn_, &QPushButton::clicked, this, &PersonTab::removeBtnClicked);
   QObject::connect(table_, &QTableWidget::itemChanged, this, &PersonTab::itemChanged);
+  table_->setContextMenuPolicy(Qt::CustomContextMenu);
+  QObject::connect(table_, &QTableWidget::customContextMenuRequested, this, &PersonTab::menu);
+
+  copy_ = new QAction("Copy", table_);
+  copy_->setShortcut(Qt::Key_Copy);
+  copy_->setEnabled(false);
+  QObject::connect(copy_, &QAction::triggered, this, &PersonTab::copy);
+
+  del_ = new QAction("Del", table_);
+  del_->setShortcut(Qt::Key_Delete);
+  del_->setEnabled(false);
+  QObject::connect(del_, &QAction::triggered, this, &PersonTab::del);
+
+  cut_ = new QAction("Cut", table_);
+  cut_->setShortcut(Qt::Key_Cut);
+  cut_->setEnabled(false);
+  QObject::connect(cut_, &QAction::triggered, this, &PersonTab::cut);
+
+  delRows_ = new QAction("Del Rows", table_);
+  delRows_->setEnabled(false);
+  QObject::connect(delRows_, &QAction::triggered, this, &PersonTab::delRows);
+
+  menu_ = std::make_unique< QMenu >(table_);
+  menu_->addAction(copy_);
+  menu_->addAction(del_);
+  menu_->addAction(cut_);
+  menu_->addAction(delRows_);
+}
+
+void PersonTab::menu(const QPoint &pos)
+{
+  checkCopyEnabled();
+  checkDelEnabled();
+  checkCutEnabled();
+  checkDelRowsEnabled();
+  menu_->exec(QCursor::pos());
+}
+
+void PersonTab::copy()
+{
+  QString str;
+  auto item = table_->currentItem();
+  if (item)
+    str = item->text();
+  QGuiApplication::clipboard()->setText(str);
+}
+
+void PersonTab::del()
+{
+  auto selectedRanges = table_->selectedRanges();
+  for (const auto &range : selectedRanges)
+  {
+    for (int col = range.leftColumn(); col <= range.rightColumn(); ++col)
+    {
+      if (col == Column::ID)
+        continue;
+      for (int row = range.topRow(); row <= range.bottomRow(); ++row)
+      {
+        if (!table_->item(row, col))
+          continue;
+        table_->item(row, col)->setText("");
+      }
+    }
+  }
+}
+
+void PersonTab::cut()
+{
+  copy();
+  del();
+}
+
+void PersonTab::delRows()
+{
+  std::map< Row_t, Id_t > rowIdMap;
+  auto selectedRanges = table_->selectedRanges();
+  for (const auto &range : selectedRanges)
+  {
+    for (Row_t row = range.bottomRow(); row >= range.topRow(); --row)
+    {
+      bool ok = true;
+      assert(("ID Column must be not null", table_->item(row, Column::ID)));
+      Id_t id = table_->item(row, Column::ID)->text().toLong(&ok);
+      assert(("ID Column must contain valid number", ok));
+      rowIdMap.insert({ row, id });
+    }
+  }
+  for (const auto [row, id] : rowIdMap | std::views::reverse)
+  {
+    try
+    {
+      std::cerr << "id " << id << " row " << row << '\n'; 
+      DbApi::removePerson(id);
+      table_->removeRow(row);
+      cachedItems_.erase(row);
+    }
+    catch (const odb::exception &e)
+    {
+      QMessageBox::warning(table_, "Skipping person",
+        QString::fromStdString("Can not delete person with id " + std::to_string(id)) + ". " + e.what(),
+        QMessageBox::Close);
+    }
+  }
 }
 
 void PersonTab::refreshTable()
@@ -84,38 +196,6 @@ void PersonTab::clearTable()
   for (auto row = table_->rowCount() - 1; row >= 0; --row)
     table_->removeRow(row);
   updateCache();
-}
-
-PersonTab::~PersonTab()
-{ }
-
-void PersonTab::removeBtnClicked()
-{
-  if (!removePersonDialog_)
-    removePersonDialog_ = std::make_unique< RemovePersonDialog >(table_);
-  auto result = removePersonDialog_->exec();
-  if (result == QDialog::Accepted)
-  {
-    bool ok = true;
-    Person::Id_t id = removePersonDialog_->getId().toLong(&ok);
-    if (!ok)
-    {
-      QMessageBox::critical(table_, "Bad ID", "Can not convert given ID to number", QMessageBox::Close);
-      return removeBtnClicked();
-    }
-    try
-    {
-      DbApi::removePerson(id);
-      QMessageBox::information(table_, "Success!", "Person removed", QMessageBox::Ok);
-    }
-    catch (const odb::exception &e)
-    {
-      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
-      return removeBtnClicked();
-    }
-    emit refreshTablesSig();
-  }
-  return;
 }
 
 void PersonTab::itemChanged(QTableWidgetItem *item)
@@ -182,6 +262,67 @@ void PersonTab::updateCache()
   }
 }
 
+void PersonTab::checkCopyEnabled()
+{
+  auto selectedRanges = table_->selectedRanges();
+  if (selectedRanges.size() != 1 || selectedRanges[0].columnCount() * selectedRanges[0].rowCount() != 1)
+    copy_->setEnabled(false);
+  else
+    copy_->setEnabled(true);
+}
+
+void PersonTab::checkDelEnabled()
+{
+  auto selectedRanges = table_->selectedRanges();
+  del_->setEnabled(!selectedRanges.empty());
+}
+
+void PersonTab::checkCutEnabled()
+{
+  cut_->setEnabled(copy_->isEnabled());
+}
+
+void PersonTab::checkDelRowsEnabled()
+{
+  auto selectedRanges = table_->selectedRanges();
+  bool allRows = true;
+  for (const auto &range : selectedRanges)
+  {
+    allRows = allRows && range.leftColumn() == Column::ID && range.rightColumn() == Column::FATHER_NAME;
+  }
+  delRows_->setEnabled(allRows);
+  del_->setEnabled(!delRows_->isEnabled());
+}
+
+void PersonTab::removeBtnClicked()
+{
+  if (!removePersonDialog_)
+    removePersonDialog_ = std::make_unique< RemovePersonDialog >(table_);
+  auto result = removePersonDialog_->exec();
+  if (result == QDialog::Accepted)
+  {
+    bool ok = true;
+    Person::Id_t id = removePersonDialog_->getId().toLong(&ok);
+    if (!ok)
+    {
+      QMessageBox::critical(table_, "Bad ID", "Can not convert given ID to number", QMessageBox::Close);
+      return removeBtnClicked();
+    }
+    try
+    {
+      DbApi::removePerson(id);
+      QMessageBox::information(table_, "Success!", "Person removed", QMessageBox::Ok);
+    }
+    catch (const odb::exception &e)
+    {
+      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
+      return removeBtnClicked();
+    }
+    emit refreshTablesSig();
+  }
+  return;
+}
+
 void PersonTab::addBtnClicked()
 {
   if (!addPersonDialog_)
@@ -214,3 +355,6 @@ void PersonTab::addBtnClicked()
   }
   return;
 }
+
+PersonTab::~PersonTab()
+{ }
