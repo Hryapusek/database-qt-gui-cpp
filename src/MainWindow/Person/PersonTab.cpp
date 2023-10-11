f89312695ac6c8a14c0c94bab457c3ba4b08829a
Abraamyan Alexander <hryapusek@gmail.com>
2023-10-11 04:39:19 +0300

    refactor

--- src/MainWindow/Person/PersonTab.cpp
+++ src/MainWindow/Person/PersonTab.cpp
@@ -5,19 +5,14 @@
 #include "AddPersonDialog.hpp"
 #include "RemovePersonDialog.hpp"
 
-#ifndef _NDEBUG
-  #include <iostream>
-#endif
-
-#include <optional>
 #include <odb/exceptions.hxx>
 #include <QMessageBox>
 #include <QMenu>
 #include <QAction>
 #include <QGuiApplication>
 #include <QClipboard>
-#include <algorithm>
 #include <ranges>
+#include <optional>
 
 namespace
 {
@@ -72,6 +67,169 @@ PersonTab::PersonTab(QTableWidget *table, QPushButton *addBtn, QPushButton *remo
   menu_->addAction(del_);
   menu_->addAction(cut_);
   menu_->addAction(delRows_);
+}
+
+void PersonTab::refreshTable()
+{
+  decltype(DbApi::getPersons()) persons;
+  try
+  {
+    persons = DbApi::getPersons();
+  }
+  catch (const odb::exception &e)
+  {
+    QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
+    return;
+  }
+  auto refreshRAII = startRefresh();
+  clearTable();
+  for (const auto &person : persons)
+  {
+    auto curRow = table_->rowCount();
+    table_->insertRow(table_->rowCount());
+    table_->setVerticalHeaderItem(curRow, new QTableWidgetItem(""));
+
+    auto idStr = QString::fromStdString(std::to_string(person.id()));
+    auto idItem = new QTableWidgetItem(idStr);
+    idItem->setFlags(idItem->flags() &  ~Qt::ItemIsEditable);
+    table_->setItem(curRow, Column::ID, idItem);
+    if (person.firstName())
+    {
+      auto firstName = QString::fromStdString(person.firstName().get());
+      table_->setItem(curRow, Column::FIRST_NAME, new QTableWidgetItem(firstName));
+    }
+    if (person.lastName())
+    {
+      auto lastName = QString::fromStdString(person.lastName().get());
+      table_->setItem(curRow, Column::LAST_NAME, new QTableWidgetItem(lastName));
+    }
+    if (person.fatherName())
+    {
+      auto fatherName = QString::fromStdString(person.fatherName().get());
+      table_->setItem(curRow, Column::FATHER_NAME, new QTableWidgetItem(fatherName));
+    }
+  }
+  updateCache();
+}
+
+void PersonTab::clearTable()
+{
+  table_->clearContents();
+  for (auto row = table_->rowCount() - 1; row >= 0; --row)
+    table_->removeRow(row);
+  updateCache();
+}
+
+void PersonTab::removeBtnClicked()
+{
+  if (!removePersonDialog_)
+    removePersonDialog_ = std::make_unique< RemovePersonDialog >(table_);
+  auto result = removePersonDialog_->exec();
+  if (result == QDialog::Accepted)
+  {
+    bool ok = true;
+    Person::Id_t id = removePersonDialog_->getId().toLong(&ok);
+    if (!ok)
+    {
+      QMessageBox::critical(table_, "Bad ID", "Can not convert given ID to number", QMessageBox::Close);
+      return removeBtnClicked();
+    }
+    try
+    {
+      DbApi::removePerson(id);
+      QMessageBox::information(table_, "Success!", "Person removed", QMessageBox::Ok);
+    }
+    catch (const odb::exception &e)
+    {
+      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
+      return removeBtnClicked();
+    }
+    emit refreshTablesSig();
+  }
+  return;
+}
+
+void PersonTab::addBtnClicked()
+{
+  if (!addPersonDialog_)
+    addPersonDialog_ = std::make_unique< AddPersonDialog >(table_);
+  auto result = addPersonDialog_->exec();
+  if (result == QDialog::Accepted)
+  {
+    std::optional< std::string > err;
+    Person person = personFromString(
+      addPersonDialog_->getFirstName(),
+      addPersonDialog_->getLastName(),
+      addPersonDialog_->getFatherName(),
+      err);
+    if (err)
+    {
+      QMessageBox::critical(table_, "Error", QString::fromStdString(*err), QMessageBox::Close);
+      return addBtnClicked();
+    }
+    try
+    {
+      DbApi::addPerson(person);
+      QMessageBox::information(table_, "Success!", "Person added", QMessageBox::Ok);
+    }
+    catch (const odb::exception &e)
+    {
+      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
+      return addBtnClicked();
+    }
+    emit refreshTablesSig();
+  }
+  return;
+}
+
+void PersonTab::itemChanged(QTableWidgetItem *item)
+{
+  if (refreshing_)
+    return;
+  assert(("Item must be not null", item));
+  auto &currentItemMeta = cachedItems_[item->row()][item->column()];
+  if (currentItemMeta.text == item->text())
+    return;
+  auto restoreText =
+    [&currentItemMeta, item]() {
+      item->setText(currentItemMeta.text);
+    };
+  assert(("Item must be not null", table_->item(item->row(), Column::ID)));
+
+  bool ok = true;
+  auto id = table_->item(item->row(), Column::ID)->text().toLong(&ok);
+  assert(("ID column must contain a valid long int", ok == true));
+  QString firstName;
+  if (table_->item(item->row(), Column::FIRST_NAME))
+    firstName = table_->item(item->row(), Column::FIRST_NAME)->text();
+
+  QString lastName;
+  if (table_->item(item->row(), Column::LAST_NAME))
+    lastName = table_->item(item->row(), Column::LAST_NAME)->text();
+
+  QString fatherName;
+  if (table_->item(item->row(), Column::FATHER_NAME))
+    fatherName = table_->item(item->row(), Column::FATHER_NAME)->text();
+  std::optional< std::string > err;
+  Person person = personFromString(std::move(firstName), std::move(lastName), std::move(fatherName), err);
+  person.id(id);
+  if (err)
+  {
+    QMessageBox::critical(table_, "Error", QString::fromStdString(*err), QMessageBox::Close);
+    restoreText();
+    return;
+  }
+  try
+  {
+    DbApi::updatePerson(std::move(person));
+    currentItemMeta.text = item->text();
+  }
+  catch (const odb::exception &e)
+  {
+    QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
+    restoreText();
+    return;
+  }
 }
 
 void PersonTab::menu(const QPoint &pos)
@@ -136,7 +294,6 @@ void PersonTab::delRows()
   {
     try
     {
-      std::cerr << "id " << id << " row " << row << '\n'; 
       DbApi::removePerson(id);
       table_->removeRow(row);
       cachedItems_.erase(row);
@@ -146,121 +303,6 @@ void PersonTab::delRows()
       QMessageBox::warning(table_, "Skipping person",
         QString::fromStdString("Can not delete person with id " + std::to_string(id)) + ". " + e.what(),
         QMessageBox::Close);
-    }
-  }
-}
-
-void PersonTab::refreshTable()
-{
-  decltype(DbApi::getPersons()) persons;
-  try
-  {
-    persons = DbApi::getPersons();
-  }
-  catch (const odb::exception &e)
-  {
-    QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
-    return;
-  }
-  auto refreshRAII = startRefresh();
-  clearTable();
-  for (const auto &person : persons)
-  {
-    auto curRow = table_->rowCount();
-    table_->insertRow(table_->rowCount());
-    table_->setVerticalHeaderItem(curRow, new QTableWidgetItem(""));
-
-    auto idStr = QString::fromStdString(std::to_string(person.id()));
-    auto idItem = new QTableWidgetItem(idStr);
-    idItem->setFlags(idItem->flags() &  ~Qt::ItemIsEditable);
-    table_->setItem(curRow, Column::ID, idItem);
-    if (person.firstName())
-    {
-      auto firstName = QString::fromStdString(person.firstName().get());
-      table_->setItem(curRow, Column::FIRST_NAME, new QTableWidgetItem(firstName));
-    }
-    if (person.lastName())
-    {
-      auto lastName = QString::fromStdString(person.lastName().get());
-      table_->setItem(curRow, Column::LAST_NAME, new QTableWidgetItem(lastName));
-    }
-    if (person.fatherName())
-    {
-      auto fatherName = QString::fromStdString(person.fatherName().get());
-      table_->setItem(curRow, Column::FATHER_NAME, new QTableWidgetItem(fatherName));
-    }
-  }
-  updateCache();
-}
-
-void PersonTab::clearTable()
-{
-  table_->clearContents();
-  for (auto row = table_->rowCount() - 1; row >= 0; --row)
-    table_->removeRow(row);
-  updateCache();
-}
-
-void PersonTab::itemChanged(QTableWidgetItem *item)
-{
-  if (refreshing_)
-    return;
-  assert(("Item must be not null", item));
-  auto &currentItemMeta = cachedItems_[item->row()][item->column()];
-  if (currentItemMeta.text == item->text())
-    return;
-  auto restoreText =
-    [&currentItemMeta, item]() {
-      item->setText(currentItemMeta.text);
-    };
-  assert(("Item must be not null", table_->item(item->row(), Column::ID)));
-
-  bool ok = true;
-  auto id = table_->item(item->row(), Column::ID)->text().toLong(&ok);
-  assert(("ID column must contain a valid long int", ok == true));
-  QString firstName;
-  if (table_->item(item->row(), Column::FIRST_NAME))
-    firstName = table_->item(item->row(), Column::FIRST_NAME)->text();
-
-  QString lastName;
-  if (table_->item(item->row(), Column::LAST_NAME))
-    lastName = table_->item(item->row(), Column::LAST_NAME)->text();
-
-  QString fatherName;
-  if (table_->item(item->row(), Column::FATHER_NAME))
-    fatherName = table_->item(item->row(), Column::FATHER_NAME)->text();
-  std::optional< std::string > err;
-  Person person = personFromString(std::move(firstName), std::move(lastName), std::move(fatherName), err);
-  person.id(id);
-  if (err)
-  {
-    QMessageBox::critical(table_, "Error", QString::fromStdString(*err), QMessageBox::Close);
-    restoreText();
-    return;
-  }
-  try
-  {
-    DbApi::updatePerson(std::move(person));
-    currentItemMeta.text = item->text();
-  }
-  catch (const odb::exception &e)
-  {
-    QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
-    restoreText();
-    return;
-  }
-}
-
-void PersonTab::updateCache()
-{
-  cachedItems_.clear();
-  for (int row = 0; row < table_->rowCount(); ++row)
-  {
-    for (int col = 0; col < table_->columnCount(); ++col)
-    {
-      if (table_->item(row, col))
-        cachedItems_[row][col] = { table_->item(row, col)->text() }
-      ;
     }
   }
 }
@@ -297,66 +339,18 @@ void PersonTab::checkDelRowsEnabled()
   del_->setEnabled(!delRows_->isEnabled());
 }
 
-void PersonTab::removeBtnClicked()
+void PersonTab::updateCache()
 {
-  if (!removePersonDialog_)
-    removePersonDialog_ = std::make_unique< RemovePersonDialog >(table_);
-  auto result = removePersonDialog_->exec();
-  if (result == QDialog::Accepted)
+  cachedItems_.clear();
+  for (int row = 0; row < table_->rowCount(); ++row)
   {
-    bool ok = true;
-    Person::Id_t id = removePersonDialog_->getId().toLong(&ok);
-    if (!ok)
+    for (int col = 0; col < table_->columnCount(); ++col)
     {
-      QMessageBox::critical(table_, "Bad ID", "Can not convert given ID to number", QMessageBox::Close);
-      return removeBtnClicked();
+      if (table_->item(row, col))
+        cachedItems_[row][col] = { table_->item(row, col)->text() }
+      ;
     }
-    try
-    {
-      DbApi::removePerson(id);
-      QMessageBox::information(table_, "Success!", "Person removed", QMessageBox::Ok);
-    }
-    catch (const odb::exception &e)
-    {
-      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
-      return removeBtnClicked();
-    }
-    emit refreshTablesSig();
   }
-  return;
-}
-
-void PersonTab::addBtnClicked()
-{
-  if (!addPersonDialog_)
-    addPersonDialog_ = std::make_unique< AddPersonDialog >(table_);
-  auto result = addPersonDialog_->exec();
-  if (result == QDialog::Accepted)
-  {
-    std::optional< std::string > err;
-    Person person = personFromString(
-      addPersonDialog_->getFirstName(),
-      addPersonDialog_->getLastName(),
-      addPersonDialog_->getFatherName(),
-      err);
-    if (err)
-    {
-      QMessageBox::critical(table_, "Error", QString::fromStdString(*err), QMessageBox::Close);
-      return addBtnClicked();
-    }
-    try
-    {
-      DbApi::addPerson(person);
-      QMessageBox::information(table_, "Success!", "Person added", QMessageBox::Ok);
-    }
-    catch (const odb::exception &e)
-    {
-      QMessageBox::critical(table_, "Error", e.what(), QMessageBox::Close);
-      return addBtnClicked();
-    }
-    emit refreshTablesSig();
-  }
-  return;
 }
 
 PersonTab::~PersonTab()
